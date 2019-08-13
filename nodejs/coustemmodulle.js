f05e8b2d8feea8769e9b44d7ab9c7829573f8188
abhinavDivakaran <abhinavabhi2461@gmail.com>
2019-08-13 15:02:54 +0530

    maths operation

---
+++ nodejs/coustemmodulle.js
@@ -0,0 +1,20 @@
+module.exports.add=(a,b)=>{
+    var c=a+b;
+    return c;
+}
+
+
+module.exports.sub=(a,b)=>{
+    var c=a-b;
+    return c;
+}
+
+module.exports.mul=(a,b)=>{
+    var c=a*b;
+    return c;
+}
+
+module.exports.div=(a,b)=>{
+    var c=a/b;
+    return c;
+}
---
+++ nodejs/modulesum.js
@@ -0,0 +1,19 @@
+const op=require('./coustemmodulle')
+
+var x=10;
+var y=2;
+
+
+
+var z=op.add(x,y);
+var z1=op.sub(x,y);
+var z2=op.mul(x,y);
+var z3=op.div(x,y);
+
+
+
+
+console.log(z);
+console.log(z1);
+console.log(z2);
+console.log(z3);
